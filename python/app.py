1ca6fd300e7d6fb3b9789ec02711844df9e8b91a
Ajaymagar <ajaymagar4818@gmail.com>
2020-10-02 16:38:34 +0530

    css

--- python/app.py
+++ python/app.py
@@ -31,9 +31,9 @@ print(ajay.person_data())
 print(person.getpopulation())
 '''
 
-'''
-#map function
 
+#map function
+'''
 list_1 = [1,2,3,4,5,6,7,8]
 
 def func(x):
@@ -42,18 +42,18 @@ def func(x):
 print(list(map(func,list_1)))
 '''
 
-'''
-# lambda function 
 
+# lambda function 
+'''
 a = [1,2,3,4,5,6,7,8,9]
 
 ajay = list(map(lambda x:x+5 ,a))
 print(ajay)
 '''
 
-'''
-# collection module 
 
+# collection module 
+'''
 import collections
 from collections import Counter
 from collections import namedtuple
@@ -65,7 +65,7 @@ print(newP)
 '''
 
 # deque module
-
+'''
 import collections
 from collections import deque
 
@@ -74,6 +74,60 @@ d.append('4')
 d.append(5)
 
 print(d)
+
+'''
+
+# Unpacking a sequence of tuple into variables 
+'''
+ajay = (4,5)
+x , y = ajay
+#print(x)
+#print(y)
+
+s = "hello"
+a , b , c ,d,e = s
+
+'''
+
+# unpacking elements from iterables of Arbitrary Length
+# star expression in python
+''' 
+record = ('ajay' , 'juinager' , '9594157161', '797724706')
+name , adderss , *phone_numbers = record
+
+print(phone_numbers)
+'''
+
+'''
+*trailing_qtrs ,current_qtr = [10,20,30,40,50,60,70,80]
+trailing_avg = sum(trailing_qtrs)/len(trailing_qtrs)
+print(trailing_avg , current_qtr)
+'''
+
+#line spilliting
+
+line2 = """ ajay magar:magarajay538,
+pari shendkar : parishendkar@gmail.com
+"""
+
+line = 'noboby:*:-2:-2:unpriviledge User:/var/emapty:/usr/bin'
+uname , *fields , homedir ,sh = line.split(':')
+#print(uname)
+#print(fields)
+first_name , *last_name = line2.split(':')
+#print(first_name)
+#print(last_name)
+
+record = ('ACME' , 50 , 123.45,(12,18,2012))
+name  , *_ , (*_ , year) = record
+print(name)
+print(year)
+
+
+
+
+
+
 
 
 
